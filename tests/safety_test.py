57cc69c4a16139d84ee0be4d3bcf9f6be7c80942
Lucas Cimon <lucas.cimon@gmail.com>
2019-04-27 22:04:54 +0200

    Pleasing pylint

--- tests/safety_test.py
+++ tests/safety_test.py
@@ -2,9 +2,9 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-from pre_commit_hooks.safety_check import main as safety
-
 import pytest
+
+from pre_commit_hooks.safety_check import main as safety
 
 
 def test_dev_requirements():
