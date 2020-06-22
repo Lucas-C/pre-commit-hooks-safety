c10ad74010c04cdcfee019da4c5c0e11fbebd686
Thomas Grainger <tagrain@gmail.com>
2020-06-22 21:15:14 +0100

    parse args with argparse

--- pre_commit_hooks/safety_check.py
+++ pre_commit_hooks/safety_check.py
@@ -1,17 +1,42 @@
 from __future__ import print_function
+
+import argparse
 import sys
+
 from safety.cli import check
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--full-report",
+    default="--full-report",
+    const="--full-report",
+    action="store_const",
+)
+parser.add_argument(
+    "--short-report", dest="full_report", const="--short-report", action="store_const"
+)
+parser.add_argument("--ignore", action="append")
+parser.add_argument("files", nargs="*", action="append")
+
+
 def main(argv):
+    ns, rest = parser.parse_known_args(["safety_check"] + args)
+    ignore = [
+        arg
+        for vs in ns.ignore
+        for code in vs.split(",")
+        for arg in ("--ignore", arg)
+    ]
+    files = [arg for f in ns.files for arg in ("--file", f)]
+    args = ([ns.full_report] + ignore + files + rest)
     try:
-        check.main(['--full-report'] + sum(([arg] if arg.startswith('-') else ['--file', arg] for arg in argv), []))
-        return 0
+        check.main(args)
     except SystemExit as error:
         if error.code == 0:
             return 0
-        return 1
+    return 1
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main(sys.argv[1:]))
